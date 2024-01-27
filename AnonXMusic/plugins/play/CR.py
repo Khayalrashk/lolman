20d0f40b35d3e6d17acd986c6b83b2fbe3b1a58f
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-01-27 06:14:52 +0200

    Update CR.py

--- AnonXMusic/plugins/play/CR.py
+++ AnonXMusic/plugins/play/CR.py
@@ -1,17 +1,12 @@
 import asyncio
-
-import os
-import time
-import requests
-from config import START_IMG_URL
 from pyrogram import filters
-import random
-from pyrogram import Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from strings.filters import command
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from AnonX import app
-from random import  choice, randint
+from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
+from pyrogram.types import Message
+
+from AnonXMusic import app
+from AnonXMusic.utils.database import set_cmode
+from AnonXMusic.utils.decorators.admins import AdminActual
+from config import BANNED_USERS
 
                 
 @app.on_message(

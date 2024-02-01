149fa7d6d231bc81b85f4426b5d5f66ae21b5e3f
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-02-01 20:56:37 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -7,22 +7,25 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "10823881"))
+API_HASH = getenv("API_HASH", '339886e2109eb67203ce12022b32e035')
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://islam2005:islam2005@cluster0.rdjot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 240))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("OWNER_ID", ""))
+
+# A name for your Music bot.
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6456857472))
+OWNER_ID = int(getenv("OWNER_ID", 5675627801))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,15 +35,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lotusabdo/Lotus.git",
+    "https://github.com/o0ooo0oo/arab1",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/C6_6V1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/l2_2Y")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,21 +81,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/e5274b67b2d75460299a5.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/e5274b67b2d75460299a5.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-STATS_IMG_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-STREAM_IMG_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/e5274b67b2d75460299a5.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
 
 def time_to_seconds(time):
